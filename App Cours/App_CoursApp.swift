0f6a0d1df0b16d481778573987c7815d64a8edbf
Axel <66967731+imxale@users.noreply.github.com>
2023-11-20 11:27:31 +0100

    Initial Commit

---
+++ App Cours/App_CoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_CoursApp.swift
+//  App Cours
+//
+//  Created by Axel Blanchard on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct App_CoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App Cours/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App Cours
+//
+//  Created by Axel Blanchard on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
